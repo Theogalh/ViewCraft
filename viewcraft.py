bce38b66b5fc8032f87b93b266b3ec6a3536aa6f
Theogalh <thomas.bouillon@epitech.eu>
2019-06-04 13:50:41 +0200

    Fix viewcraft.py for uwsgi

--- viewcraft.py
+++ viewcraft.py
@@ -1,9 +1,12 @@
 from app import create_app, db
 from app.models import User, Post, Character, Roster, Guild
 
-app = create_app()
+application = create_app()
 
 
-@app.shell_context_processor
+@application.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Character': Character, 'Roster': Roster, 'Guild': Guild}
+
+if __name__ == "__main__":
+    application.run()
